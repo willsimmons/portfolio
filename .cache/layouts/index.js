46a4840c5e6f514dd90ce43a39244bfab8739c49
willsimmons <willsimmons@gmail.com>
2018-02-23 17:55:57 -0800

    removal of cache
    moving cache to git ignore no need for it in repo

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/will/Projects/portfolio/src/layouts/index.js"
-  import data from "/Users/will/Projects/portfolio/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
