a20da1f06a2f6df73deae66cc9aace7b23bf6e4e
Will Simmons <willsimmons@gmail.com>
2018-03-14 11:46:00 -0700

    headers, redirects, favicon
    header changes, redirects and favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,24 @@ module.exports = {
         accessToken: `${process.env.CONTENT_TOKEN}` || '',
       },
     },
+      {
+          resolve: `gatsby-plugin-favicon`,
+          options: {
+              logo: "./src/assets/images/favicon.png",
+              injectHTML: true,
+              icons: {
+                  android: true,
+                  appleIcon: true,
+                  appleStartup: true,
+                  coast: false,
+                  favicons: true,
+                  firefox: true,
+                  twitter: false,
+                  yandex: false,
+                  windows: false
+              }
+          }
+      },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
